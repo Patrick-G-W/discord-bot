e3bea454877c6e5404ec3bc9067c6b8143e6accf
Patrick-G-W <patrick.g.ward@protonmail.com>
2019-03-23 09:53:47 +0000

    Improved IMDb performance, leave it for now

--- movie_tv_search.py
+++ movie_tv_search.py
@@ -7,15 +7,15 @@ ia = IMDb()
 def search(message):
     movieSearch = message[6:]
     movie = ia.search_movie(movieSearch)
-    movieTitle = str(movie[0])
     movieID = movie[0].movieID
+    getMovie = ia.get_movie(movieID)
+    movieTitle = getMovie['title']
     movieURL = ia.get_imdbURL(movie[0])
-    movieThumbURL = (ia.get_movie(movieID))['cover url']
-    moviePlot = str((ia.get_movie(movieID))['plot'][0])
-    movieRelease = ia.get_movie(movieID)['year']
-    movieScore = ia.get_movie(movieID)['rating']
-    #movieGenre = ia.get_movie(movieID)['genres'].join(",")
-    movieGenre = ", ".join(ia.get_movie(movieID)['genres'])
+    movieThumbURL = getMovie['cover url']
+    moviePlot = str(getMovie['plot'][0])
+    movieRelease = getMovie['year']
+    movieScore = getMovie['rating']
+    movieGenre = ", ".join(getMovie['genres'])
     limitMoviePlot = moviePlot[:1000] + (moviePlot[1000:] and '...')
     return movieTitle, movieURL, movieThumbURL, limitMoviePlot, movieRelease, movieScore, movieGenre
 
